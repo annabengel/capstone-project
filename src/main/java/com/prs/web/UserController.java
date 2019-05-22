28c139ab99a24aea52519b0413f7fae700316597
annabengel <annambengel@gmail.com>
2019-05-22 11:17:42 -0400

    first capstone push

---
+++ src/main/java/com/prs/web/UserController.java
@@ -0,0 +1,30 @@
+package com.prs.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prs.business.JsonResponse;
+import com.prs.db.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@GetMapping("/")
+	public JsonResponse getAll() {
+		JsonResponse jr = null;
+		try {
+			jr = JsonResponse.getInstance(userRepo.findAll());
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+
+	}
+
+}
